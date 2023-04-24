03153b216cf8113021bf1022044667bc41f6121e
Joel Kanerva <joel@infograafikko.fi>
2023-04-24 16:18:06 +0300

    url fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  base:"https://infograafikko.github.io/2023-04-hy_palveluportfolio/dist/",
   plugins: [solidPlugin()],
   server: {
     port: 3000,
